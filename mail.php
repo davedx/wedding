85e14bfd7c4053ae5df1397818c0247afd6622a8
Dave Clayton <davedx@gmail.com>
2016-12-07 19:38:13 +0100

    Latest including mail script

---
+++ mail.php
@@ -0,0 +1,21 @@
+<?php
+if (isset($_REQUEST['name_1']))  {
+
+  $name_1 = $_REQUEST['name_1'];
+  $status = $_REQUEST['status'];
+  $starter_1 = $_REQUEST['starter_1'];
+  $main_1 = $_REQUEST['main_1'];
+  $starter_2 = $_REQUEST['starter_2'];
+  $main_2 = $_REQUEST['main_2'];
+
+  $message = "From: $name_1\n\nStatus: $status\n\n";
+  $message .= "Starter_1: $starter_1\nMain_1: $main_1\n\n";
+  $message .= "Starter_2: $starter_2\nMain_2: $main_2\n\n";
+
+  //send email
+  mail("davedx@gmail.com", "Wedding RSVP", $message, "From: automailer@laradave.nl");
+
+  header("Location: http://laradave.nl/index.html#rsvp_thanks");
+  die();
+}
+?>
