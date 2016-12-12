a5009c11290b2b3e90f8ce8ebe09cfb6982dac10
Dave Clayton <dave@vandebron.nl>
2016-12-12 20:45:22 +0100

    Errors and stuff

--- mail.php
+++ mail.php
@@ -3,12 +3,16 @@ if (isset($_REQUEST['name_1']))  {
 
   $name_1 = $_REQUEST['name_1'];
   $status = $_REQUEST['status'];
+  $type = $_REQUEST['type'];
+  $number_of_evening_guests = $_REQUEST['number_of_evening_guests'];
   $starter_1 = $_REQUEST['starter_1'];
   $main_1 = $_REQUEST['main_1'];
   $starter_2 = $_REQUEST['starter_2'];
   $main_2 = $_REQUEST['main_2'];
 
   $message = "From: $name_1\n\nStatus: $status\n\n";
+  $message .= "Status: $type\n";
+  $message .= "Number of evening guests: $number_of_evening_guests\n";
   $message .= "Starter_1: $starter_1\nMain_1: $main_1\n\n";
   $message .= "Starter_2: $starter_2\nMain_2: $main_2\n\n";
 
